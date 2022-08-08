4001c287ac03d6ed8d01a8db261c3ef3c4def44e
David AAC <daalvarez17@utpl.edu.ec>
2022-08-07 19:19:23 -0500

    batch con emuladores

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,46 +4,17 @@ import * as functions from 'firebase-functions';
 import * as firebaseAdmin from 'firebase-admin';
 import 'firebase-functions';
 import {generateMock} from '@anatine/zod-mock';
-import {z} from 'zod';
 import {orderBaseSchema} from './schema';
 
 firebaseAdmin.initializeApp();
 const db = firebaseAdmin.firestore();
+
 db.settings({
   timestampsInSnapshots: true,
+  ignoreUndefinedProperties: true,
 });
-
-const schema = z.object({
-  id: z.number().min(11).max(40),
-  date: z.date(),
-  items: z.number().min(11).max(40),
-  total: z.number().min(18).max(120),
-  subtotal: z.number().min(18).max(120),
-  client: z.string().nonempty(),
-  referralCode: z.string().nonempty(),
-  factura: z.boolean(),
-  formaPago: z.enum(['light', 'dark']),
-  totalSinImpuestos: z.number().min(18).max(120),
-  totalDescuento: z.number().min(18).max(120),
-  currency: z.enum(['USD', 'BTC', 'ETH']),
-
-});
-
-// console.log(generateMock(schema));
-const mockData = generateMock(schema);
+const batch = db.batch();
 const order = generateMock(orderBaseSchema);
-const orderOK = {
-  date: order.date,
-  client: order.client,
-  orderNumber: order.orderNumber,
-  creationDeviceId: order.creationDeviceId,
-  formaPago: order.formaPago,
-  totalSinImpuestos: order.totalSinImpuestos,
-  currency: order.currency,
-  totalConImpuestos: order.totalConImpuestos,
-  pagos: order.pagos,
-};
-
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info('Hello logs!', {structuredData: true});
@@ -52,20 +23,9 @@ export const helloWorld = functions.https.onRequest((request, response) => {
 
 
 export const writeOrder = functions.https.onRequest((req, res)=> {
-  db.doc(`orders/${mockData.id}`).create({
-    date: order.date,
-    // items: order.items.items,
-    client: order.client,
-    orderNumber: order.orderNumber,
-    creationDeviceId: order.creationDeviceId,
-    formaPago: order.formaPago,
-    totalSinImpuestos: order.totalSinImpuestos,
-    currency: order.currency,
-    totalConImpuestos: order.totalConImpuestos,
-    pagos: order.pagos,
-
-  });
+  db.doc(`orders/${order.orderNumber}`).create(order);
   console.log('Orden agregada');
+
   res.send(200);
 });
 // db.batch()
@@ -96,11 +56,22 @@ export const deleteOrder = functions.https.onRequest((req, res)=> {
 });
 
 
-const batch = db.batch();
-
 export const newPopulate = functions.https.onRequest((req, res) => {
-  const docRef = db.doc(`orders/${orderOK.orderNumber}`);
-  batch.create(docRef, orderOK);
+  const docRef = db.doc(`orders/${order.orderNumber}`);
+  batch.create(docRef, order);
   batch.commit();
   res.send('Hello from Firebase!');
+});
+
+exports.addOrder = functions.https.onRequest(async (req, res)=>{
+  for (let i = 0; i <= 5; i++) {
+    const order = generateMock(orderBaseSchema);
+
+    order.orderNumber = i;
+    const docRef = db.collection('orders').doc(`${order.orderNumber}`);
+
+    batch.set(docRef, order);
+  }
+  await batch.commit();
+  res.status(200).json({successfull: true});
 });
